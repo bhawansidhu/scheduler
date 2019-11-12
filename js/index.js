9b5eb43fbb9cce33ec5abf2325e27be3c12b0bd9
Bhawanpreet Sidhu <n01372787@nj-00118-014.humber.org>
2019-11-12 12:22:06 -0500

    use array

--- js/index.js
+++ js/index.js
@@ -7,36 +7,41 @@ const courseBreaks = true; // Boolean
 const courseDuration = 160; // Minutes per class
 
 
-const course = {
-  name: `Tools and Workflow`,
-  code: `WDDM-115`,
-  instructor: `Kadeem Best`,
-  start: { term: `Fall`, year: 2019 },
-  weeks: 15,
-  breaks: true,
-  duration: 160
-};
+const allCourses =[
+  {
+      name: `Tools and Workflow`,
+      code: `WDDM-115`,
+      instructor: `Kadeem Best`,
+      start: { term: `Fall`, year: 2019 },
+      weeks: 15,
+      breaks: true,
+      duration: 160
+  },{
+      name: `Planning & Prototyping`,
+      code: `WDDM-117`,
+      instructor: `Cory Coletta`,
+      start: { term: `Fall`, year: 2019 },
+      weeks: 15,
+      breaks: true,
+      duration: 160
 
-document.getElementById(`name`).innerHTML = course.name;
-document.getElementById(`code`).innerHTML = course.code;
-document.getElementById(`instructor`).innerHTML = course.instructor;
-document.getElementById(`start`).innerHTML +=  `Hi! ${course.start.term} ${course.start.year}`;
-document.getElementById(`weeks`).innerHTML = course.weeks;
+  }
+] 
+
+document.getElementById(`name`).innerHTML = allCourses[0].name;
+document.getElementById(`code`).innerHTML = allCourses[0].code;
+document.getElementById(`instructor`).innerHTML = allCourses[0].instructor;
+document.getElementById(`start`).innerHTML +=  `Hi! ${allCourses[0].start.term} ${allCourses[0].start.year}`;
+document.getElementById(`weeks`).innerHTML = allCourses[0].weeks;
 document.getElementById(`duration`).innerHTML = getDurationFromMinutes(160);  // 2 hr, 40 min
-/*
-LAB:
-1. Fix the output of the start term by turning the Object's properties into a String
-      Ex: { term: `Fall`, year: 2019 }  >>  `Fall 2019`
-2. Write a function that will take a Number of minutes and will return a human-readable String
-      Ex: 160  >>  `2 hr, 40 min`
-      Hints:
-        - Division: /
-        - Remainder: %
-        - Rounding:
-            - Standard rules:     Math.round(4.5)
-            - Always round up:    Math.ceil(1.001)
-            - Always round down:  Math.floor(1.999)
-*/
+
+// LAB: ARRAYS, PART 1
+  // 1. Create a 2nd course Object
+  // 2. Store both course Objects in an array whose reference is stored as: allCourses
+  //      allCourses is an [] (reference) that hold two identically structured Objects
+  // 3. Modify the lines above (11-16) to put the values from the first course into the document
+  // 4. (Later) Move the HTML for course into a Javascript function that returns a formatted HTML string
+  
 
 
 const start = { term: `Fall`, year: 2019 };
